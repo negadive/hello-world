5be94adba3c8d8bafeab52f6e7dd5ddf3fda1de3
Teguh <putr4.g4ul@gmail.com>
2020-05-20 22:03:16 +0700

    init

---
+++ cobain.py
@@ -0,0 +1,10 @@
+print("belajar conflict")
+
+#header {
+#    width: 100%;
+#    height: 50px;
+#    line-height: 50px;
+#    background-color: burlywood;
+#    text-align: center;
+#    color: white;
+# }
